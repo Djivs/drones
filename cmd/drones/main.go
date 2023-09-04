5c7decfd6078856881610cda1dafde4427e31850
Djivs <icemcpe099@gmail.com>
2023-09-05 00:30:42 +0300

    first commit

---
+++ cmd/drones/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"drones/internal/api"
+)
+
+func main() {
+	log.Println("Application start!")
+	api.StartServer()
+	log.Println("Application terminated!")
+
+}
---
+++ internal/api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartServer() {
+	log.Println("Server start up")
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.LoadHTMLGlob("../../templates/*")
+
+	r.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	r.Static("/image", "../../resources")
+	r.Run()
+
+	log.Println("Server down")
+}
