a94db8392af7613e964a3ac231f3419addeec975
Djivs <icemcpe099@gmail.com>
2023-09-07 00:23:52 +0300

    load cards using templates library

--- internal/api/server.go
+++ internal/api/server.go
@@ -7,6 +7,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Card struct {
+	Title       string
+	Text        string
+	Button_text string
+	Image       string
+}
+
 func StartServer() {
 	log.Println("Server start up")
 
@@ -18,15 +25,24 @@ func StartServer() {
 		})
 	})
 
-	r.LoadHTMLGlob("../../templates/*")
+	r.LoadHTMLGlob("../../templates/*.html")
+	r.Static("/image", "../../resources")
+	r.Static("/node_modules", "../../node_modules")
+
+	cards := []Card{
+		{"Title 1", "Text 1", "Button_text 1", "image/image.jpg"},
+		{"Title 2", "Text 2", "Button_text 2", "image/image.jpg"},
+		{"Title 3", "Text 3", "Button_text 3", "image/image.jpg"},
+		{"Title 4", "Text 4", "Button_text 4", "image/image.jpg"},
+	}
 
 	r.GET("/home", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Main website",
+			"title": "Main Page",
+			"cards": cards,
 		})
 	})
 
-	r.Static("/image", "../../resources")
 	r.Run()
 
 	log.Println("Server down")
