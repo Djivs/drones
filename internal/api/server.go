0fe788351e759c91e3cbfcd589276c5770e31f9f
Djivs <icemcpe099@gmail.com>
2023-09-12 23:25:58 +0300

    consider remarks

--- internal/api/server.go
+++ internal/api/server.go
@@ -9,25 +9,27 @@ import (
 )
 
 type Card struct {
-	Title   string
-	Text    string
-	Image   string
-	Details string
+	Title      string
+	Text       string
+	Image      string
+	Details    string
+	AreaKm     float64
+	Population int
 }
 
 var cards = []Card{
-	{"Кузьминки", "ЮВАО", "image/kuzminki.jpg", "Заявка для Кузьминок"},
-	{"Люберцы", "МО", "image/lyubertsi.jpg", "Заявка для Люберец"},
-	{"Савёловский", "САО", "image/savyolovsky.jpg", "Заявка для Савёловского"},
-	{"Строгино", "СЗАО", "image/strogino.jpg", "Заявка для Строгино"},
+	{"Кузьминки", "ЮВАО", "image/kuzminki.jpg", "Район с красивым парком и добрыми людьми", 8.15, 140670},
+	{"Люблино", "ЮВАО", "image/lyublino.jpg", "Район с прекрасным Люблинским парком и богатым культурным наследием", 17.41, 181447},
+	{"Савёловский", "САО", "image/savyolovsky.jpg", "Район, в котором располагается Савёловский вокзал", 2.7, 60070},
+	{"Строгино", "СЗАО", "image/strogino.jpg", "Район Москвы с красивыми водоёмами и живописными берегами", 16.84, 161980},
 }
 
 func StartServer() {
 	log.Println("Server start up")
+
 	r := gin.Default()
 
 	r.LoadHTMLGlob("../../templates/*.html")
-
 	r.Static("/image", "../../resources/image")
 	r.Static("/css", "../../templates/css")
 	r.Static("/font", "../../resources/font")
@@ -44,7 +46,7 @@ func loadHome(c *gin.Context) {
 	card_title := c.Query("card_title")
 
 	if card_title == "" {
-		c.HTML(http.StatusOK, "index.html", gin.H{
+		c.HTML(http.StatusOK, "regions.html", gin.H{
 			"cards": cards,
 		})
 		return
@@ -58,10 +60,8 @@ func loadHome(c *gin.Context) {
 		}
 	}
 
-	c.HTML(http.StatusOK, "search.html", gin.H{
-		"cards":       foundCards,
-		"Amount":      len(foundCards),
-		"Search_text": card_title,
+	c.HTML(http.StatusOK, "regions.html", gin.H{
+		"cards": foundCards,
 	})
 }
 
@@ -70,11 +70,13 @@ func loadPage(c *gin.Context) {
 
 	for i := range cards {
 		if cards[i].Title == title {
-			c.HTML(http.StatusOK, "page.html", gin.H{
-				"Title":   cards[i].Title,
-				"Image":   "../" + cards[i].Image,
-				"Text":    cards[i].Text,
-				"Details": cards[i].Details,
+			c.HTML(http.StatusOK, "region.html", gin.H{
+				"Title":      cards[i].Title,
+				"Image":      "../" + cards[i].Image,
+				"Text":       cards[i].Text,
+				"Details":    cards[i].Details,
+				"AreaKm":     cards[i].AreaKm,
+				"Population": cards[i].Population,
 			})
 			return
 		}
