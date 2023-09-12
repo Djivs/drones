1821f94c14b63a43ced790e83a6ec4e42ab15649
Djivs <icemcpe099@gmail.com>
2023-09-12 20:42:25 +0300

    remove lambdas

--- internal/api/server.go
+++ internal/api/server.go
@@ -15,18 +15,16 @@ type Card struct {
 	Details string
 }
 
+var cards = []Card{
+	{"Кузьминки", "ЮВАО", "image/kuzminki.jpg", "Заявка для Кузьминок"},
+	{"Люберцы", "МО", "image/lyubertsi.jpg", "Заявка для Люберец"},
+	{"Савёловский", "САО", "image/savyolovsky.jpg", "Заявка для Савёловского"},
+	{"Строгино", "СЗАО", "image/strogino.jpg", "Заявка для Строгино"},
+}
+
 func StartServer() {
 	log.Println("Server start up")
-	cards := []Card{
-		{"Кузьминки", "ЮВАО", "image/kuzminki.jpg", "Заявка для Кузьминок"},
-		{"Люберцы", "МО", "image/lyubertsi.jpg", "Заявка для Люберец"},
-		{"Савёловский", "САО", "image/savyolovsky.jpg", "Заявка для Савёловского"},
-		{"Строгино", "СЗАО", "image/strogino.jpg", "Заявка для Строгино"},
-	}
-
 	r := gin.Default()
-
-	r.GET("/ping", ping)
 
 	r.LoadHTMLGlob("../../templates/*.html")
 
@@ -34,53 +32,51 @@ func StartServer() {
 	r.Static("/css", "../../templates/css")
 	r.Static("/font", "../../resources/font")
 
-	r.GET("/", func(c *gin.Context) {
-		card_title := c.Query("card_title")
-
-		if card_title == "" {
-			c.HTML(http.StatusOK, "index.html", gin.H{
-				"cards": cards,
-			})
-			return
-		}
-
-		foundCards := []Card{}
-		lowerCardTitle := strings.ToLower(card_title)
-		for i := range cards {
-			if strings.Contains(strings.ToLower(cards[i].Title), lowerCardTitle) {
-				foundCards = append(foundCards, cards[i])
-			}
-		}
-
-		c.HTML(http.StatusOK, "search.html", gin.H{
-			"cards":       foundCards,
-			"Amount":      len(foundCards),
-			"Search_text": card_title,
-		})
-
-	})
-	r.GET("/:title", func(c *gin.Context) {
-		title := c.Param("title")
-
-		for i := range cards {
-			if cards[i].Title == title {
-				c.HTML(http.StatusOK, "page.html", gin.H{
-					"Title":   cards[i].Title,
-					"Image":   "../" + cards[i].Image,
-					"Text":    cards[i].Text,
-					"Details": cards[i].Details,
-				})
-			}
-		}
-	})
+	r.GET("/", loadHome)
+	r.GET("/:title", loadPage)
 
 	r.Run(":8000")
 
 	log.Println("Server down")
 }
 
-func ping(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
+func loadHome(c *gin.Context) {
+	card_title := c.Query("card_title")
+
+	if card_title == "" {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"cards": cards,
+		})
+		return
+	}
+
+	foundCards := []Card{}
+	lowerCardTitle := strings.ToLower(card_title)
+	for i := range cards {
+		if strings.Contains(strings.ToLower(cards[i].Title), lowerCardTitle) {
+			foundCards = append(foundCards, cards[i])
+		}
+	}
+
+	c.HTML(http.StatusOK, "search.html", gin.H{
+		"cards":       foundCards,
+		"Amount":      len(foundCards),
+		"Search_text": card_title,
 	})
+}
+
+func loadPage(c *gin.Context) {
+	title := c.Param("title")
+
+	for i := range cards {
+		if cards[i].Title == title {
+			c.HTML(http.StatusOK, "page.html", gin.H{
+				"Title":   cards[i].Title,
+				"Image":   "../" + cards[i].Image,
+				"Text":    cards[i].Text,
+				"Details": cards[i].Details,
+			})
+			return
+		}
+	}
 }
