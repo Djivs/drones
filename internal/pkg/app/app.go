2a4dc66e959d3161221f4f007e7c03703a6a5a1c
dj1vs <datrifonov03@gmail.com>
2023-10-06 00:32:08 +0300

    start lab3

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -10,6 +10,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type PingRequestBody struct {
+	Message string
+}
+
 type Application struct {
 	repo repository.Repository
 	r    *gin.Engine
@@ -30,84 +34,23 @@ func (a *Application) StartServer() {
 	log.Println("Server started")
 
 	a.r = gin.Default()
-
-	a.r.LoadHTMLGlob("../../templates/*.html")
-	a.r.Static("/css", "../../templates/css")
-	a.r.Static("/js", "../../templates/js")
-
-	a.r.GET("/", a.loadRegions)
-	a.r.GET("/:region_name", a.loadRegion)
-	a.r.POST("/delete_region/:region_name", a.loadRegionChangeVisibility)
+	a.r.GET("ping", ping)
 
 	a.r.Run(":8000")
 
 	log.Println("Server is down")
 }
 
-func (a *Application) loadRegions(c *gin.Context) {
-	region_name := c.Query("region_name")
+func ping(c *gin.Context) {
+	var requestBody PingRequestBody
 
-	if region_name == "" {
-		all_regions, err := a.repo.GetAllRegions()
-
-		if err != nil {
-			log.Println(err)
-			c.Error(err)
-		}
-
-		c.HTML(http.StatusOK, "regions.html", gin.H{
-			"regions": all_regions,
-		})
-	} else {
-		found_regions, err := a.repo.SearchRegions(region_name)
-
-		if err != nil {
-			c.Error(err)
-			return
-		}
-
-		c.HTML(http.StatusOK, "regions.html", gin.H{
-			"regions":     found_regions,
-			"Search_text": region_name,
-		})
-	}
-}
-
-func (a *Application) loadRegion(c *gin.Context) {
-	region_name := c.Param("region_name")
-
-	if region_name == "favicon.ico" {
-		return
+	if err := c.BindJSON(&requestBody); err != nil {
+		// DO SOMETHING WITH THE ERROR
 	}
 
-	region, err := a.repo.GetRegionByName(region_name)
+	log.Println(requestBody.Message)
 
-	if err != nil {
-		c.Error(err)
-		return
-	}
-
-	c.HTML(http.StatusOK, "region.html", gin.H{
-		"Name":           region.Name,
-		"Image":          region.Image,
-		"AreaKm":         region.AreaKm,
-		"Population":     region.Population,
-		"Details":        region.Details,
-		"HeadName":       region.HeadName,
-		"HeadEmail":      region.HeadEmail,
-		"HeadPhone":      region.HeadPhone,
-		"AverageHeightM": region.AverageHeightM,
-		"Region_status":  region.Status,
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
 	})
-}
-
-func (a *Application) loadRegionChangeVisibility(c *gin.Context) {
-	region_name := c.Param("region_name")
-	err := a.repo.ChangeRegionVisibility(region_name)
-
-	if err != nil {
-		c.Error(err)
-	}
-
-	c.Redirect(http.StatusFound, "/"+region_name)
 }
